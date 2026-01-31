0c4ee8fc8d86b5e8478b973630110e6f7c9edd9d
Yogitha <pendemyogitha4@gmail.com>
2026-01-31 10:04:27 +0530

    first commit

---
+++ src/main/java/com/cg/EmployeeManagementSystem_Boot_MVC/repository/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package com.cg.EmployeeManagementSystem_Boot_MVC.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.EmployeeManagementSystem_Boot_MVC.entity.Employee;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+
+	
+}
